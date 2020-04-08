b9e2b09e3b8fd3d6ff8516fe5399302b7b7d7768
P!eum <jednitan10@gmail.com>
2020-04-08 17:59:52 +0700

    Test project page

--- src/ProjectSchedulingApplication.java
+++ src/ProjectSchedulingApplication.java
@@ -4,7 +4,7 @@ public class ProjectSchedulingApplication {
     private static Scanner scanner = new Scanner(System.in);
     private static TaskManager taskManager = new TaskManager();
 
-    
+
     private static boolean addNewTask(){
         System.out.print("Task Name: ");
         String taskName = scanner.nextLine();
@@ -25,22 +25,42 @@ public class ProjectSchedulingApplication {
         return true;
     }
 
+    private static void projectPage(){
+        System.out.println("Project: ");
+        System.out.println("1. Edit Project Information");
+        System.out.println("2. Add New Task");
+        System.out.println("3. Select Task");
+        System.out.println("4. Print Schedule Report");
+        System.out.println("5. Save & Exit");
+        System.out.print("Enter: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        switch (choice){
+            case 2:
+                addNewTask();
+                break;
+            case 3:
+                taskManager.showAllTask();
+                break;
+        }
+    }
+
 
 
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Welcome to Project Scheduling Application");
         System.out.println("1. Create New Project");
         System.out.println("2. Load Project");
         System.out.print("Enter: ");
         int choice = scanner.nextInt();
+        scanner.nextLine();
         switch (choice){
-            case(1):
+            case 1:
                 System.out.println(" Create New Project");
                 createNewProject();
                 break;
-            case(2):
+            case 2:
                 System.out.println("Load Project");
                 loadProject();
                 break;
