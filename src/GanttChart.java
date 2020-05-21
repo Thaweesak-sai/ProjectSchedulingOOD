2fe54b275b64807d27800d8f9f0e1055be9e1570
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2020-05-21 19:44:04 +0700

    Finish GanttChart with JTable

--- src/GanttChart.java
+++ src/GanttChart.java
@@ -1,3 +1,5 @@
+
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -9,50 +11,55 @@ public class GanttChart {
     private LocalDate projectEndDate;
     private LocalDate projectStartDate;
     JFrame frame;
+    JTable table;
     public GanttChart(Project project){
         this.project = project;
         this.projectEndDate=project.getEndDate();
         this.projectStartDate=project.getStartDate();
     }
 
-    public void getGanttChart()
-    {
+    public void getGanttChart() {
+         frame = new JFrame();
+         frame.setTitle("Project : "+project.getName() +" Gantt Chart");
         TaskManager taskManager = project.getTaskManager();
         List<Task> task = taskManager.getAllTask();
-        /*assign value*/
-        ArrayList<ArrayList<String>> ganttChartt = new ArrayList<>();
-        ganttChartt.add(new ArrayList<>());
-        ganttChartt.get(0).add("DATE");
-        List<LocalDate> dateInBetween = getDateInBetween(projectStartDate,projectEndDate);
-        for(int i=0;i<dateInBetween.size();i++)
+        List<LocalDate> projectRange = getDateInBetween(projectStartDate, projectEndDate);
+        String[] column = new String[projectRange.size()+1];
+        column[0] = "DATE : ";
+        for(int i =1;i<projectRange.size()+1;i++)
         {
-            ganttChartt.get(0).add(DateFormatter.formatDateToStringForDisplay(dateInBetween.get(i)));
+            column[i] = DateFormatter.formatDateToStringForDisplay(projectRange.get(i-1));
         }
-        int tableSize = ganttChartt.get(0).size();
-        for(int row =1,i=0;row<task.size()+1;row++,i++)
+        String[][] data = new String[task.size()][projectRange.size()+1];
+        System.out.println(projectRange);
+        System.out.println(column.length);
+        for(int i =0;i<task.size();i++)
         {
-            /* initialize arrayList size to be the number of date in between project*/
-            ganttChartt.add(new ArrayList<String>(Collections.nCopies(tableSize,"---------------")));
-            /* assign task name to the first element of each row*/
-            ganttChartt.get(row).set(0,task.get(i).getTaskName());
             List<LocalDate> taskRange = getDateInBetween(task.get(i).getStartDate(),task.get(i).getEndDate());
-            for(int j=0;j<taskRange.size();j++) {
-                if (dateInBetween.contains(taskRange.get(j))) {
-                    int indexWorked = dateInBetween.indexOf(taskRange.get(j)) + 1;
-                    ganttChartt.get(row).set(indexWorked, "XXXXXXXXXXXXXXX");
+            System.out.println("task "+task.get(i).getTaskName()+"Range : "+taskRange);
+            data[i][0] = task.get(i).getTaskName();
+            for(int j=1;j<column.length;j++)
+            {
+                data[i][j] = "-"; /* initialize all table's cell with '-'*/
+                for(int k=0;k<taskRange.size();k++)
+                {
+                    if (DateFormatter.formatDateToStringForDisplay(taskRange.get(k)).equals(column[j]))
+                    {
+                        data[i][j] = "X";
+                        if(taskRange.get(k).getDayOfWeek()== DayOfWeek.SATURDAY||taskRange.get(k).getDayOfWeek()==DayOfWeek.SUNDAY)
+                        {
+                            data[i][j] = "-";
+                        }
+                    }
                 }
             }
         }
-        for(int i=0;i<ganttChartt.size();i++)
-        {
-            System.out.println(ganttChartt.get(i));
-        }
-
-        /* Wait for motivation,  Jframe + Table Studying.*/
-//        ArrayList<String> taskName = new ArrayList<>(ganttChartt.get(0));
-//        System.out.println(taskName);
-//        frame = new JFrame();
-//        JTable jt = new JTable(ganttChartt,taskName);
+        table = new JTable(data,column);
+        table.setBounds(30,40,200,300);
+        JScrollPane sp = new JScrollPane(table);
+        frame.add(sp);
+        frame.setSize(500,200);
+        frame.setVisible(true);
     }
     /**
      * getDateInBetween(). A method of list all the dates in between two dates
@@ -61,7 +68,7 @@ public class GanttChart {
     {
         List<LocalDate> inBetweenDate = new ArrayList<>();
         LocalDate date;
-        for(date = startDate;!date.isEqual(endDate);date = date.plusDays(1))
+        for(date = startDate;!date.isAfter(endDate);date = date.plusDays(1))
         {
             inBetweenDate.add(date);
         }
